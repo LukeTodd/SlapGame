c7494daa7f110e20f70f3043bb9751ff676195e9
Luke Todd <luke.todd22@yahoo.com>
2019-02-08 20:14:53 -0700

    added structure and js

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
-var health = 100
+let health = 100
 
-function slap() {
-  return health - 1
+function slap(val) {
+  health = health - val;
+  if (health < 0) {
+    health = 0
+  }
+  document.getElementById('display-hp').innerText = health.toString()
 }
+
+function defaultDisplay() {
+  health = 100
+  document.getElementById('display-hp').innerText = health.toString()
+}
+
+defaultDisplay()
