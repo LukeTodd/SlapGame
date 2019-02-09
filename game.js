fca03bac6913445db92b4447a94b3c86fe9f5fc7
Luke Todd <luke.todd22@yahoo.com>
2019-02-08 23:40:57 -0700

    more styling

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 let health = 100
 
+let steroids = {
+  name: 'steroids',
+  modifier: 2,
+  description: 'makes you strong'
+}
+
 function slap(val) {
   health = health - val;
   if (health < 0) {
@@ -7,6 +13,16 @@ function slap(val) {
   }
   document.getElementById('display-hp').innerText = health.toString()
 }
+
+function feed(val) {
+  health = health + val;
+  if (health > 100) {
+    health = 100
+  }
+  document.getElementById('display-hp').innerText = health.toString()
+}
+
+
 
 function defaultDisplay() {
   health = 100
